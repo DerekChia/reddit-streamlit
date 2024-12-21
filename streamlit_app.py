7e89e026dfdf1c5c5ab18e43eccd7d27e76021e4
Derek Chia <derek@derekchia.com>
2024-12-21 14:06:15 +0800

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,14 @@
 import streamlit as st
+import clickhouse_connect
+import os
 
-st.write("Hello world")
+client = clickhouse_connect.get_client(
+    host=os.environ["CLICKHOUSE_HOST"],
+    port=os.environ["PORT"],
+    username=os.environ["CLICKHOUSE_USERNAME"],
+    password=os.environ["CLICKHOUSE_PASSWORD"],
+)
+
+res = client.query("select 1")
+
+st.write(res)
